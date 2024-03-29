d6d1f19ffc6fc82825aa79eed0f98e4d0e71c5e6
DESKTOP-2CI28UR\Ashan <ashan.f@knightsbridge.tech>
2024-03-29 15:37:51 +0530

    font chnages in layout page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -9,12 +9,12 @@ const page = () => {
                 <div className="container-xl lg:container m-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
                     <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-                        <h2 className="text-2xl font-bold">Redeem Vouchers</h2>                        
+                        <h2 className="text-2xl font-bold flex items-center justify-center">Redeem Vouchers</h2>                        
                     </div>
                     
                     <a href="/report">
-                        <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-                            <h2 className="text-2xl font-bold">View Reports</h2>
+                        <div className="bg-gray-100 p-6 rounded-lg shadow-md ">
+                            <h2 className="text-2xl font-bold flex items-center justify-center">View Reports</h2>
                         </div>
                     </a>
                     </div>
