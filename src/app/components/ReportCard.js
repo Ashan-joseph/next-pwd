c9343d4b8bd1982884eb111ba681633ba07c7259
DESKTOP-2CI28UR\Ashan <ashan.f@knightsbridge.tech>
2024-04-05 10:11:36 +0530

    report change

--- src/app/components/ReportCard.js
+++ src/app/components/ReportCard.js
@@ -5,16 +5,13 @@ const ReportCard = ({voucher}) => {
     return (
         <>            
             <div className="bg-gray-100 p-6 rounded-lg shadow-md mb-4">
-                <h2 className="text-2xl font-bold text-center">Ref No: {voucher.voucher_ref_no}</h2>                   
+                <h2 className="text-2xl font-bold text-center">Ref No: {voucher.voucher_ref_no}</h2> <hr />               
                     <div>                        
-                        <div className="mt-2 mb-4 text-center">
-                            Product Type: {voucher.product_name}<br/>
-                            Merchant: {voucher.merchant}<br/>
-                            Denomination: {voucher.denomination}<br/>
-                            Location: {voucher.redeem_location}<br/>
-                            Redeem date: {voucher.redeemed_date}<br/>
-                            Reference No: {voucher.transaction_ref_no}<br/>
-                            {/* Created date: {voucher.created_at} */}
+                        <div className="mt-2 mb-4 text-center">                           
+                                <div>Product Type: {voucher.product_name}</div>                                   
+                                <div className="col-start-3">Denomination: {voucher.denomination}</div>                                    
+                                <div>Reference No: {voucher.transaction_ref_no}</div>
+                                <div>Redeem date: {voucher.redeemed_date}</div>                      
                         </div>
                     </div>     
             </div>           
